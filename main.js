c7999dfbe8877b017039ad38492119faeefaf7d5
enmanuelbayen <enmanuel0495@gmail.com>
2023-04-24 13:16:51 -0400

    Added add/remove function

--- main.js
+++ main.js
@@ -13,14 +13,39 @@ let bookList = [
     }
 ];
 
-const addBook = function (title, author) {
-    bookList.push({
-        'title': title,
-        'author': author
-    });
-};
+// const addBook = function (title, author) {
+//     bookList.push({
+//         'title': title,
+//         'author': author
+//     });
+// };
 
-const removeButtons = document.querySelectorAll('.remove');
-const removeBook = function() {
-    bookList.filter()
-}
+const addBook = document.querySelector('.addBttn');
+const remove = document.querySelectorAll('.removeBttn');
+const bookBody = document.querySelector('.bookList-container'); 
+
+addBook.addEventListener('click', () => {
+    const shelf = document.createElement('div');
+    shelf.className = 'shelfBox';
+    shelf.innerHTML = `
+    <div class="book-title-box">
+        <h2 class="book-title">${bookList[i].title}</h2>
+    </div>
+    <div class="author-box">
+        <h2 class="book-author">${bookList[i].author}</h2>
+    </div>
+    <button class="removeBttn">Remove</button>
+    `;
+
+    bookBody.appendChild(shelf);
+})
+
+// const removeButtons = document.querySelectorAll('.remove');
+// const removeBook = function() {
+//     bookList.filter(bookList[1], -1);
+// }
+
+remove.addEventListener('click', () => {
+    bookList.filter(bookList[i], -1);
+    bookBody.removeChild(shelf);
+})
