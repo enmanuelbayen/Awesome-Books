f842a84491737801d855811b66476e09db019bae
enmanuelbayen <enmanuel0495@gmail.com>
2023-04-24 16:22:16 -0400

    Added new books object/html

--- main.js
+++ main.js
@@ -1,57 +1,19 @@
 let bookList = [
-    {
+    {   
         'title': 'Lorem Ipsum',
         'author': 'Testeroo testyy'   
-    },
-    {
-        'title': 'Lorem Ipsum2',
-        'author': 'Testeroo testyy' 
-    },
-    {
-        'title': 'Lorem Ipsum3',
-        'author': 'Testeroo testyy'
     }
 ];
 
-// const addBook = function (title, author) {
-//     bookList.push({
-//         'title': title,
-//         'author': author
-//     });
-// };
 
-const addBook = document.querySelector('.addBttn');
+const add = document.querySelector('.addBttn');
 const remove = document.querySelectorAll('.removeBttn');
 const bookBody = document.querySelector('.bookList-container'); 
-
-addBook.addEventListener('click', () => {
-    const shelf = document.createElement('div');
-    shelf.className = 'shelfBox';
-    shelf.innerHTML = `
-    <div class="book-title-box">
-        <h2 class="book-title">${bookList[i].title}</h2>
-    </div>
-    <div class="author-box">
-        <h2 class="book-author">${bookList[i].author}</h2>
-    </div>
-    <button class="removeBttn">Remove</button>
-    `;
-
-    bookBody.appendChild(shelf);
-})
-
-// const removeButtons = document.querySelectorAll('.remove');
-// const removeBook = function() {
-//     bookList.filter(bookList[1], -1);
-// }
-
-//remove.addEventListener('click', () => {
-    //bookList.filter(bookList[i], -1);
-    //bookBody.removeChild(shelf);
-//})
-
+let title = document.querySelector('.book-title');
+let author = document.querySelector('.book-author');
 
 const printBooks = function() {
+    bookBody.innerHTML=``;
     for (let i=0; i<bookList.length; i+=1){
         const shelf = document.createElement('div');
         shelf.className = 'shelfBox';
@@ -68,4 +30,20 @@ const printBooks = function() {
     }
 }
 
-printBooks();
+add.addEventListener('click', function () {
+    let newObj = {
+        'title': title.value,
+        'author': author.value,
+    };
+    
+    bookList.push(newObj);
+    printBooks();
+    return bookList;
+});
+
+
+
+remove.addEventListener('click', () => {
+    bookList.filter(bookList[i], -1);
+    bookBody.removeChild(shelf);
+})
