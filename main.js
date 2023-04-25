4ea00ee39aa46ebc011f642feec2c6fbe304b3fe
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-25 17:04:03 -0300

    changes to css and js
    background of shelfbox change to intercalate between gray and white.
    commented code in js delete.
    title of books now appear in quotation marks
    when add button is clicked the value in the form is reset.

--- main.js
+++ main.js
@@ -1,59 +1,3 @@
-// let bookList = [];
-
-// if (localStorage.getItem('data')) {
-//   bookList = JSON.parse(localStorage.getItem('data'));
-// }
-
-// const add = document.querySelector('.addBttn');
-// const bookBody = document.querySelector('.bookList-container');
-// const title = document.querySelector('.book-title');
-// const author = document.querySelector('.book-author');
-
-// // Function to add to the html and object array
-
-// const printBooks = () => {
-//   bookBody.innerHTML = '';
-//   for (let i = 0; i < bookList.length; i += 1) {
-//     const shelf = document.createElement('div');
-//     shelf.className = 'shelfBox';
-//     shelf.innerHTML = `<div class="book-title-box">
-//             <h2 class="book-title">${bookList[i].title}</h2>
-//         </div>
-//         <div class="author-box">
-//             <h2 class="book-author">${bookList[i].author}</h2>
-//         </div>
-//         <button class="removeBttn">Remove</button>`;
-
-//     bookBody.appendChild(shelf);
-//   }
-// };
-
-// add.addEventListener('click', () => {
-//   const newObj = {
-//     title: title.value,
-//     author: author.value,
-//   };
-
-//   bookList.push(newObj);
-//   localStorage.setItem('data', JSON.stringify(bookList));
-//   printBooks();
-// });
-
-// // Remove function
-
-// bookBody.addEventListener('click', (event) => {
-//   if (event.target.classList.contains('removeBttn')) {
-//     const indexToRemove = Array.from(bookBody.children).indexOf(event.target.parentElement);
-
-//     bookList.splice(indexToRemove, 1);
-//     localStorage.setItem('data', JSON.stringify(bookList));
-
-//     event.target.parentElement.remove();
-//   }
-// });
-
-// printBooks();
-
 // class for Awesome books
 
 class BookList {
@@ -76,7 +20,7 @@ class BookList {
       const shelf = document.createElement('div');
       shelf.className = 'shelfBox';
       shelf.innerHTML = `<div class="book-title-box">
-              <h2 class="book-title">${this.bookList[i].title}</h2>
+              <h2 class="book-title">'${this.bookList[i].title}'</h2>
               <span> by </span>
               <h2 class="book-author">${this.bookList[i].author}</h2>
           </div>
@@ -93,6 +37,8 @@ class BookList {
     this.bookList.push(newObj);
     localStorage.setItem('data', JSON.stringify(this.bookList));
     this.printBooks();
+    this.title.value= '';
+    this.author.value = '';
   }
 
   removeBook(event) {
