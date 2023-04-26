e0f7a2ba7ca11862c097b836e8eefcba1c9de20a
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-26 13:33:35 -0300

    js functions add
    functions to show and hide sections create

--- main.js
+++ main.js
@@ -73,3 +73,26 @@ const contactNav = document.getElementById('conctact-list');
 const booklistSection = document.getElementById('landing-page');
 const addbookSection = document.getElementById('add-book');
 const contactSection = document.getElementById('contact-section');
+
+const showList = () =>{
+  booklistSection.classList.remove('hide');
+  addbookSection.classList.add('hide');
+  contactSection.classList.add('hide');
+}
+
+const showAddbook = ()=>{
+  booklistSection.classList.add('hide');
+  addbookSection.classList.remove('hide');
+  contactSection.classList.add('hide');
+}
+
+const showContact = ()=>{
+  booklistSection.classList.add('hide');
+  addbookSection.classList.add('hide');
+  contactSection.classList.remove('hide');
+}
+
+
+listNav.addEventListener('click', showList);
+addNewNav.addEventListener('click', showAddbook);
+contactNav.addEventListener('click', showContact);
